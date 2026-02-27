bd83a09f065b41e03406f47fd4e1c989f1b50446
DaniloEstacio-hub <danilomoises99@gmail.com>
2026-02-26 21:23:14 -0300

    Update xadrez.c
    feito incremento "VOID"

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,48 @@
 #include <stdio.h>
 
+ void bispo(int b){
+if (b>0)
+   {
+       printf(" Bispo superior Direita. \n");
+
+    bispo(b-1);
+   }
+}
+//Torre a direita
+void torre(int t){
+if (t>0)
+   {
+       printf(" Torre a direita. \n");
+
+    bispo(t-1);
+   }
+}
+
+// rainha
+void rainha(int r){
+if (r>0)
+   {
+       printf(" Rainha a direita. \n");
+
+    rainha(r-1);
+   }
+}
 
 
 int main() {
-   
-    int bispo=0; 
-//Bispo: 5 casas na diagonal superior direita
- while(bispo < 5)
- {
-printf("Bispo Superior direita \n");
-
-bispo++;
- }
-
+  
+bispo (5);
+ 
  printf("\n");
 
-//Torre: 5 casas para a direita
-int torre=0;
-do
-
-{
-    printf("Torre a direita\n");
-
-    torre++;
-} while (torre < 5);
+torre (5);
 
 printf("\n");
 
-//Rainha: 8 casas para a esquerda
-
-for(int rainha=0; rainha < 8; rainha++)
-
-{
-    printf("Rainha a esquerda\n");
-
-}
+rainha(5);
 
 printf("\n");
  
-  // movimento cavalo 
   for(int i=1;i <=1; i++){
 
 int j=1;    
@@ -51,13 +55,5 @@ int j=1;
    
 printf("Cavalo Direita\n");
   }
-
-   
-
-
-
-
-
-
     return 0;
 }
